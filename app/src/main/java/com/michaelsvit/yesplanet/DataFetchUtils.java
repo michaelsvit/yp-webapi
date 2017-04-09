08cbc4b1e082248a8bb39943cf9a5b94ff37cd0a
Michael Svit <michaelsvit@gmail.com>
2017-04-09 14:22:55 +0300

    Add documentation

--- app/src/main/java/com/michaelsvit/yesplanet/DataFetchUtils.java
+++ app/src/main/java/com/michaelsvit/yesplanet/DataFetchUtils.java
@@ -13,6 +13,11 @@ import okhttp3.Request;
 public abstract class DataFetchUtils {
     private final static String LOG_TAG = DataFetchUtils.class.getSimpleName();
 
+    /**
+     * Asynchronously fetch data from given URL and execute callback method on completion.
+     * @param url       url of data to be fetched
+     * @param callback  callback to execute when a response was acquired
+     */
     public static void fetchData(final String url, Callback callback) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
