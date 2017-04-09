bd2c20f6cf3b9de6a02d216cf6473608cacca4bd
Michael Svit <michaelsvit@gmail.com>
2017-04-09 12:50:39 +0300

    Create basic Cinema object with data url

---
+++ app/src/main/java/com/michaelsvit/yesplanet/Cinema.java
@@ -0,0 +1,16 @@
+package com.michaelsvit.yesplanet;
+
+/**
+ * Created by Michael on 4/9/2017.
+ *
+ * Object representing Yes Planet cinema
+ */
+
+public class Cinema {
+    private final String dataUrl = "http://yesplanet.internet-bee.mobi/" +
+            "TREST_YP3/resources/info/merge/0/CATS,FEAT,TIX,PRSNT";
+
+    public String getDataUrl() {
+        return dataUrl;
+    }
+}
